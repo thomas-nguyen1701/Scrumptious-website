3c98548570672bc3e478f41e1a3ede3a600f4d6c
Thomas Nguyen <thomas.nguyen1701@gmail.com>
2024-10-02 18:53:28 -0700

    :D

---
+++ scrumptious-website/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+    return(
+        <header>
+            <h1>My Website!</h1>
+            <nav>
+                <ul>
+                    <li><a href = '#'></a>Home</li>
+                    <li><a href = '#'></a>About</li>
+                    <li><a href = '#'></a>Services</li>
+                    <li><a href = '#'></a>Contact</li>
+
+                </ul>
+            </nav>
+            <hr></hr>
+        </header>
+    );
+}
+export default Header
