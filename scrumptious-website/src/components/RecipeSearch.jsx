084bcdb417dcd8d2bfafd004c5c63decc604040d
Thomas Nguyen <thomas.nguyen1701@gmail.com>
2024-10-11 22:12:58 -0700

    almost done with search bar

--- scrumptious-website/src/components/RecipeSearch.jsx
+++ scrumptious-website/src/components/RecipeSearch.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import './RecipeSearch.css'
 
 function RecipeSearch (){
     return(
         <div className= "input=wrapper">
-    <FontAwesomeIcon icon={faSearch} />    
+            <FontAwesomeIcon icon={faSearch} />
+            <input placeholder="Type to search..."/>   
+
         </div>
     );
 }
