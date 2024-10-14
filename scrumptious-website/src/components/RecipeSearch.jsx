38c8075134e859ac518d24799b7b300baabc7782
thomas-nguyen1701 <121322450+thomas-nguyen1701@users.noreply.github.com>
2024-10-14 14:30:04 -0700

    almost done with search bar

--- scrumptious-website/src/components/RecipeSearch.jsx
+++ scrumptious-website/src/components/RecipeSearch.jsx
@@ -1,12 +1,22 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import './RecipeSearch.css'
+import { useState } from 'react';
 
 function RecipeSearch (){
+    const[input, setInput] = useState("")
+
+    const FetchData = {value} => {
+        fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+    }
     return(
-        <div className= "input=wrapper">
+        <div className= "input-wrapper">
             <FontAwesomeIcon icon={faSearch} />
-            <input placeholder="Type to search..."/>   
+            <input 
+                placeholder="Type to search..." 
+                value={input} 
+                onChange={(e)=> setInput(e.target.value)}
+            />   
 
         </div>
     );
