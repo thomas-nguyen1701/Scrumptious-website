7ab59febd40ffef2ed150d7e5f70b898e24bd296
thomas-nguyen1701 <121322450+thomas-nguyen1701@users.noreply.github.com>
2024-10-15 10:02:31 -0700

    changing the searchbar

--- scrumptious-website/src/components/RecipeSearch.jsx
+++ scrumptious-website/src/components/RecipeSearch.jsx
@@ -6,16 +6,25 @@ import { useState } from 'react';
 function RecipeSearch (){
     const[input, setInput] = useState("")
 
-    const FetchData = {value} => {
+    const fetchData = (value) => {
         fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
-    }
+        .then((reponse) => reponse.json())
+        .then((json) => {
+            console.log(json);
+        });
+    
+    };
+    const handleChange = (value) => {
+        setInput(value);
+        fetchData(value);
+    };
     return(
         <div className= "input-wrapper">
             <FontAwesomeIcon icon={faSearch} />
             <input 
                 placeholder="Type to search..." 
                 value={input} 
-                onChange={(e)=> setInput(e.target.value)}
+                onChange={(e)=> handleChange(e.target.value)}
             />   
 
         </div>
